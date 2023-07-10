9bbd345fa08aab58633e6afc218db7ea621fa53e
Ebrahim-Elkbbany <ana.elkbany@gmail.com>
2023-07-11 01:40:42 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const LaborApp());
+}
+class LaborApp extends StatelessWidget {
+  const LaborApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: ,
+    );
+  }
+}
